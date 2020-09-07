493e1cef9ccede32395dabc7c6d0d988c21c4fcc
cbaker1 <cbaker1@unimelb.edu.au>
2020-09-07 19:10:15 +1000

    Improved the testing data visulaisation

--- COVID_data_Aus.py
+++ COVID_data_Aus.py
@@ -2,43 +2,40 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# def test_number_hist(url, max_num, plt_title, save_name, save_dir='Data_plots/')
-#
+def test_number_hist(url, max_num, plt_title, save_name,
+                     save_dir='Data_plots/', scaling=1.,
+                     xlabel_extra_text=''):
+    data_tests = pd.read_html(url)
+    data_net = data_tests[1]['NET']
+    data_net = [int(i) for i in data_net if i != '-']
+    data_net = np.array(data_net)*scaling
+    plt.hist(data_net, np.linspace(0, max_num, 20))
 
+    percentile_80 = np.percentile(data_net, 80)
+    print(f"{save_name} - 80th percentile: {percentile_80}")
+    plt.plot([percentile_80] * 2, plt.ylim(), '--')
+    plt.xlim(0, max_num)
+    plt.xlabel(f'Tests per day{xlabel_extra_text}')
+    plt.ylabel('Frequency')
+    plt.title(plt_title)
+    plt.savefig(f"{save_dir}{save_name}")
+    plt.close()
 
-vic_tests = pd.read_html('https://covidlive.com.au/report/daily-tests/vic')
-vic_data = vic_tests[1]['NET']
-vic_data = [int(i) for i in vic_data if i != '-']
-plt.hist(vic_data, np.arange(0, 45000, 2500))
+vec = [
+    ['vic', 45000, 'Victoria', 5000000],
+       ['qld', 30000, 'Queensland', 2500000],
+       ['nsw', 40000, 'New South Wales', 5300000]
+       ]
 
-vic_80_percentile = np.percentile(vic_data, 80)
-plt.plot([vic_80_percentile]*2, plt.ylim(),'--')
-plt.xlim(0, 45000)
-plt.title('Victoria')
-plt.show()
-plt.close()
-
-
-qld_tests = pd.read_html('https://covidlive.com.au/report/daily-tests/qld')
-qld_data = qld_tests[1]['NET']
-qld_data = [int(i) for i in qld_data if i != '-']
-plt.hist(qld_data, np.arange(0, 30000, 2000))
-
-qld_80_percentile = np.percentile(qld_data, 80)
-plt.plot([qld_80_percentile]*2, plt.ylim(),'--')
-
-plt.xlim(0, 30000)
-plt.title('Queensland')
-plt.show()
-
-nsw_tests = pd.read_html('https://covidlive.com.au/report/daily-tests/nsw')
-nsw_data = nsw_tests[1]['NET']
-nsw_data = [int(i) for i in nsw_data if i != '-']
-plt.hist(nsw_data, np.arange(0, 40000, 2000))
-
-nsw_80_percentile = np.percentile(nsw_data, 80)
-plt.plot([nsw_80_percentile]*2, plt.ylim(),'--')
-
-plt.xlim(0, 40000)
-plt.title('New South Wales')
-plt.show()
+for l in vec:
+    test_number_hist(f'https://covidlive.com.au/report/daily-tests/{l[0]}',
+                     max_num=l[1],
+                     plt_title=l[2],
+                     save_name=f'{l[0]}_absolute.png')
+    current_scale = 1000/l[3]
+    test_number_hist(f'https://covidlive.com.au/report/daily-tests/{l[0]}',
+                     max_num=10,
+                     plt_title=l[2],
+                     save_name=f'{l[0]}_per_thousand.png',
+                     scaling=current_scale,
+                     xlabel_extra_text=' per thousand')
